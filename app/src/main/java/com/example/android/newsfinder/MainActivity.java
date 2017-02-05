ecb7b26b608f0183baddea2a15f45e2a8259b846
epatter1 <elishajp20@gmail.com>
2017-02-05 14:37:23 -0500

    Added splash screen
    Added splash screen which works and about to parse from json file

---
+++ app/src/main/java/com/example/android/newsfinder/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.newsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import info.androidhive.androidsplashscreentimer.R;
+
+import static info.androidhive.androidsplashscreentimer.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+
+
+}
