bb51013fa59f067d21050b803599d353ecb48bae
epatter1 <elishajp20@gmail.com>
2017-02-06 16:15:29 -0500

    Added newspaper_locations.json file

--- app/src/main/java/com/example/android/newsfinder/MainActivity.java
+++ app/src/main/java/com/example/android/newsfinder/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.android.newsfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import info.androidhive.androidsplashscreentimer.R;
 
@@ -14,6 +26,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
     }
-
 
 }
